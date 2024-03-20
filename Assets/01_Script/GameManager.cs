a17cdd77f6b8790a1db9791aeeeb1fbe1265e34a
JDY <ejrduddlekt@naver.com>
2024-03-20 20:39:48 +0900

    게임매니저 주석 추가

--- Assets/01_Script/GameManager.cs
+++ Assets/01_Script/GameManager.cs
@@ -11,6 +11,12 @@ public class GameManager : SingletonMonobehaviour<GameManager>
 
     private void Start()
     {
+        /*
+            * ToDo
+            *  - 플레이어 세이브 로드
+            *  - 세이브가 없으면 플레이어를 등록 씬으로 리다이렉션한다.
+            *  - 백엔드를 호출하고 일일 챌린지와 보상을 얻는다.
+        */
         _sessionStartTime = DateTime.Now;
         Debug.Log("Game Session Start @ : " + DateTime.Now);
     }
